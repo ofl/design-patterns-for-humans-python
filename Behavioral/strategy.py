64adeb419a99b338a4d2558f68d27ffe2ae0a478
ofl <ofl@covered.jp>
2021-04-03 18:17:16 +0900

    戻り値を修正した #1

--- Behavioral/strategy.py
+++ Behavioral/strategy.py
@@ -10,13 +10,13 @@ class SortStrategy(metaclass=ABCMeta):
 
 
 class BubbleSortStrategy(SortStrategy):
-    def sort(self, dataset: list):
+    def sort(self, dataset: list) -> list:
         print("Sorting using bubble sort")
         return dataset
 
 
 class QuickSortStrategy(SortStrategy):
-    def sort(self, dataset: list):
+    def sort(self, dataset: list) -> list:
         print("Sorting using quick sort")
         return dataset
 
@@ -25,8 +25,8 @@ class Sorter():
     def __init__(self, sorter: SortStrategy) -> None:
         self._sorter = sorter
 
-    def sort(self, array: list):
-        self._sorter.sort(array)
+    def sort(self, array: list) -> list:
+        return self._sorter.sort(array)
 
 
 dataset = [1, 5, 4, 3, 2, 8]
