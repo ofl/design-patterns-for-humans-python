135315f394fcb3071944721ee4df5dfc16864e9c
ofl <ofl@covered.jp>
2021-04-27 19:33:05 +0900

    add Flyweight Before #2

--- Structural/flyweight_before.py
+++ Structural/flyweight_before.py
@@ -6,32 +6,19 @@ class KarakTea():
         self.name = tea_type
 
 
-class TeaMaker():
-    def __init__(self) -> None:
-        self._available_tea = {}
-
-    def make(self, preference: str):
-        if preference not in self._available_tea:
-            self._available_tea[preference] = KarakTea(preference)
-
-        return self._available_tea[preference]
-
-
 class TeaShop():
-    def __init__(self, tea_maker: TeaMaker) -> None:
-        self._tea_maker = tea_maker
+    def __init__(self) -> None:
         self._orders = {}
 
     def take_order(self, tea_type: str, table: int):
-        self._orders[table] = self._tea_maker.make(tea_type)
+        self._orders[table] = KarakTea(tea_type)
 
     def serve(self):
         for k, v in self._orders.items():
             print(f'Serving {v.name} tea to table# ' + str(k))
 
 
-tea_maker = TeaMaker()
-shop = TeaShop(tea_maker)
+shop = TeaShop()
 
 shop.take_order('less sugar', 1)
 shop.take_order('more milk', 2)
