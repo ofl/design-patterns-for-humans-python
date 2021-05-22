2836d755bcc190c41e7cb008b029a71c8caf2308
ofl <ofl@covered.jp>
2021-05-22 19:14:45 +0900

    while文を使うように修正した #2

--- Behavioral/iterator_befor.py
+++ Behavioral/iterator_befor.py
@@ -6,12 +6,30 @@ class Book():
         return self._name
 
 
-bookShelf = []
+class BookShelf():
+    def __init__(self) -> None:
+        self._books: list[Book] = []
+
+    def append(self, book: Book):
+        self._books.append(book)
+
+    def get_book_at(self, index):
+        return self._books[index]
+
+    def is_valid(self, index):
+        return 0 <= index < len(self._books)
+
+
+bookShelf = BookShelf()
 
 bookShelf.append(Book("Around the World in 80 days"))
 bookShelf.append(Book("Bible"))
 bookShelf.append(Book("Cinderella"))
 bookShelf.append(Book("Daddy-Long-Legs"))
 
-for book in bookShelf:
+index = 0
+
+while bookShelf.is_valid(index):
+    book = bookShelf.get_book_at(index)
     print(book.get_name())
+    index += 1
